a186cdcd04c8bdb315f277141614c666c9e3dec3
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-10-06 15:20:57 +0530

    Added Scripts

---
+++ Scripts/Find_Best_Model.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pickle
+import utils as u
+
+#####################################################################################################
+
+def find():
+    PATH = "C:/Users/Ourself/Desktop/Repos/PetFinder/Misc"
+
+    args_1 = "--name"
+
+    if args_1 in sys.argv: name = sys.argv[sys.argv.index(args_1) + 1]
+
+    with open(os.path.join(PATH, name + ".pkl"), "rb") as fp:
+        params = pickle.load(fp)
+    
+    RMSE = []
+    for i in range(len(params)):
+        RMSE.append(params[i]["RMSE"])
+    
+    index = RMSE.index(min(RMSE))
+
+    u.breaker()
+    print(params[index])
+    u.breaker()
+
+#####################################################################################################
