98f7637e212f46044231879fe399e12843e7db54
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-10-08 08:33:36 +0530

    Scripts Folder: Update #3

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,6 +3,7 @@ import sys
 import utils as u
 from Find_Best_Model import find
 from Calc_Avg_CV_Score import calculate
+from Colab_CV_Analyze import analyze
 
 #####################################################################################################
 
@@ -11,11 +12,14 @@ def main():
     
     args_1 = "--find"
     args_2 = "--calc"
+    args_3 = "--colab"
 
     if args_1 in sys.argv:
         find()
     if args_2 in sys.argv:
         calculate()
+    if args_3 in sys.argv:
+        analyze()
     
     u.breaker()
 
